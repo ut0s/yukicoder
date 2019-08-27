be82c95215cc5cdfd653aab1c669f65611c3cb51
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-27 11:17:07 +0900

    add: No.795 Restrictions!!!!!!!!!!!!!! | (AC) https://yukicoder.me/submissions/372770

---
+++ src/795.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 795.cpp
+  @title  No.795 Restrictions!!!!!!!!!!!!!! - yukicoder
+  @url https://yukicoder.me/problems/no/795
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  LL N, M;
+  cin >> N >> M;
+
+  cout << "Yes" << endl;
+  return 0;
+}

be3b5e7bf53b2a9296587574d432d367073c9ff4
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-28 22:04:28 +0900

    add:  No.687 E869120 | (AC) https://yukicoder.me/submissions/373199

---
+++ src/687.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 687.cpp
+  @title  No.687 E869120 and Constructing Array 1 - yukicoder
+  @url https://yukicoder.me/problems/no/687
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << N / 2 << " " << N - (N / 2) << endl;
+  return 0;
+}

03769a84e6d59d78ea58b239f8d9d5a8c6d838f7
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-28 21:57:54 +0900

    add:  No.692 square1001 | (AC) https://yukicoder.me/submissions/373194

---
+++ src/692.cpp
@@ -0,0 +1,25 @@
+/**
+  @file 692.cpp
+  @title  No.692 square1001 and Permutation 1 - yukicoder
+  @url https://yukicoder.me/problems/no/692
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  int N;
+  cin >> N;
+
+  if (N != 1) {
+    cout << "Petr" << endl;
+  } else {
+    cout << "square1001" << endl;
+  }
+
+  return 0;
+}

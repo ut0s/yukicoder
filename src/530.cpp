05aefd03f27017c55d151a45940053291e662f11
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-30 08:39:00 +0900

    add:  No.530 年齢って毎年変わるし覚えるの難しいよね | (AC) https://yukicoder.me/submissions/373491

---
+++ src/530.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 530.cpp
+  @title  No.530 年齢って毎年変わるし覚えるの難しいよね - yukicoder
+  @url https://yukicoder.me/problems/no/530
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  int Y;
+  cin >> Y;
+
+  cout << 2017 - Y << endl;
+  return 0;
+}

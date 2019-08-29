24cd67bb27f10b5aea30731a0d2b570e4ec28768
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-30 08:33:50 +0900

    add:  No.543 命題 | (AC) https://yukicoder.me/submissions/373489

---
+++ src/543.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 543.cpp
+  @title  No.543 命題 - yukicoder
+  @url https://yukicoder.me/problems/no/543
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  string P, P_;
+  cin >> P >> P_;
+
+  cout << P_ << " " << P << endl;
+  return 0;
+}

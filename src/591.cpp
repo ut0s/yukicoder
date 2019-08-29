2c84385abd106e54fb1ef66956fb870c9732b3e2
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-08-29 21:52:05 +0900

    add:  No.591 (^o^)/ | (AC) https://yukicoder.me/submissions/373415

---
+++ src/591.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 591.cpp
+  @title  No.591 (^o^)/ - yukicoder
+  @url https://yukicoder.me/problems/no/591
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  string T1, T2;
+  cin >> T1 >> T2;
+
+  cout << "(" << T1 << T2 << T1 << ")/" << endl;
+  return 0;
+}

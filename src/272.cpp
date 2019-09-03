66a1fc1d4807d32a0ab06b9c44061100a61839b9
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-03 17:17:20 +0900

    add:  No.272 NOT回路 | (AC) https://yukicoder.me/submissions/375332

---
+++ src/272.cpp
@@ -0,0 +1,21 @@
+/**
+  @file 272.cpp
+  @title  No.272 NOT回路 - yukicoder
+  @url https://yukicoder.me/problems/no/272
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+
+int main() {
+  bool X;
+  cin >> X;
+
+  cout << !X << endl;
+  return 0;
+}

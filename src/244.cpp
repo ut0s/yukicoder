6fe34ec9b02a14589270142ed2ad7900ebcaa5c9
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-03 17:36:51 +0900

    add:  No.244 ★１のグラフの問題 | (AC) https://yukicoder.me/submissions/375341

---
+++ src/244.cpp
@@ -0,0 +1,20 @@
+/**
+  @file 244.cpp
+  @title  No.244 ★１のグラフの問題 - yukicoder
+  @url https://yukicoder.me/problems/no/244
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, N) for (int i = 0; i < (N); ++i)
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << N - 1 << endl;
+  return 0;
+}

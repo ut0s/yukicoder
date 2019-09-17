8f6196fe71aa25b4016ba968f8a54294c126be9d
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-17 12:28:12 +0900

    add:  No.882 約数倍数 | (AC) https://yukicoder.me/submissions/380014

---
+++ src/882.cpp
@@ -0,0 +1,73 @@
+/**
+  @file 882.cpp
+  @title  No.882 約数倍数 - yukicoder
+  @url https://yukicoder.me/problems/no/882
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define UNIQ(x) v.erase(unique(ALL(x)), x.end());
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+template <class T>
+T gcd(T a, T b) {
+  if (b == 0)
+    return a;
+
+  return gcd(b, a % b);
+}
+
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(12);
+}
+
+int main() {
+  ::iostream_init();
+
+  int A, B;
+  cin >> A >> B;
+
+  if (gcd(A, B) == B) {
+    OUT("YES");
+  } else {
+    OUT("NO");
+  }
+
+  return 0;
+}

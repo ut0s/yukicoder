0eeac28b59b95106db017867bc3cc6b7ca8d6c9c
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-17 22:01:00 +0900

    add:  No.667 Mice's Luck(ネズミ達の運) | (AC) https://yukicoder.me/submissions/380194

---
+++ src/667.cpp
@@ -0,0 +1,67 @@
+/**
+  @file 667.cpp
+  @title  No.667 Mice's Luck(ネズミ達の運) - yukicoder
+  @url https://yukicoder.me/problems/no/667
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define UNIQ(x) v.erase(unique(ALL(x)), x.end());
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(13);
+}
+
+int main() {
+  ::iostream_init();
+
+  string S;
+  cin >> S;
+
+  int trap = count(ALL(S), 'x');
+  int N    = SZ(S);
+  REP(i, SZ(S)) {
+    OUT(100.0 * (1.0 - double(trap) / double(N - i)));
+    if (S[i] == 'x') {
+      trap--;
+    }
+  }
+
+  return 0;
+}

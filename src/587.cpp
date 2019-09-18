363e6464981b57fcce7e99891093282c162e9a90
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-19 07:32:15 +0900

    add:  No.587 七対子 | (AC) https://yukicoder.me/submissions/380627

---
+++ src/587.cpp
@@ -0,0 +1,80 @@
+/**
+  @file 587.cpp
+  @title  No.587 七対子 - yukicoder
+  @url https://yukicoder.me/problems/no/587
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define SORT(x) sort(ALL(x))
+#define UNIQ(x) x.erase(unique(ALL(x)), x.end())
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(12);
+}
+
+int main() {
+  ::iostream_init();
+
+  string S;
+  cin >> S;
+  SORT(S);
+
+  string tmp = S;
+  UNIQ(tmp);
+
+  vector<char> c;
+  if (SZ(tmp) == 7) {
+    REP(i, SZ(tmp)) {
+      if (count(ALL(S), tmp[i]) == 1) {
+        c.PB(tmp[i]);
+      }
+    }
+
+    if (SZ(c) == 1) {
+      OUT(c[0]);
+    } else {
+      OUT("Impossible");
+    }
+  } else {
+    OUT("Impossible");
+  }
+
+  return 0;
+}

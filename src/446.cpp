f8723171850cce06facc55ccb35c723f34eb911f
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-09-24 00:56:45 +0900

    add:  No.446 ゆきこーだーの雨と雪 | (AC) https://yukicoder.me/submissions/382771

---
+++ src/446.cpp
@@ -0,0 +1,70 @@
+/**
+  @file 446.cpp
+  @title  No.446 ゆきこーだーの雨と雪 (1) - yukicoder
+  @url https://yukicoder.me/problems/no/446
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define SORT(x) sort(ALL(x))
+#define UNIQ(x) x.erase(unique(ALL(x)), x.end())
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(12);
+}
+
+regex re("[0-9]+");
+
+int main() {
+  ::iostream_init();
+
+  string A, B;
+  cin >> A >> B;
+
+  auto str_i_match = [](string S) { return (regex_match(S, re) && S == to_string(stoi(S))); };
+  auto in_range    = [](int n) { return (0 <= n && n <= 12345); };
+
+  if (str_i_match(A) && str_i_match(B) && in_range(stoi(A)) && in_range(stoi(B))) {
+    OUT("OK");
+  } else {
+    OUT("NG");
+  }
+
+  return 0;
+}

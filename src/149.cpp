bfe0fe3820794b166c8dd3696ec02c750ad88372
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2019-10-12 23:44:54 +0900

    add:  No.149 碁石の移動 | (AC) https://yukicoder.me/submissions/389645

---
+++ src/149.cpp
@@ -0,0 +1,84 @@
+/**
+  @file 149.cpp
+  @title  No.149 碁石の移動 - yukicoder
+  @url https://yukicoder.me/problems/no/149
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long uLL;
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define SZ(x) ((int)(x).size())
+
+#define REPI(i, a, b) for (LL i = LL(a); i < LL(b); ++i)
+#define REP(i, N) for (LL i = LL(0); i < LL(N); ++i)
+#define REPS(i, N) for (int i = 1; i <= (int)(N); i++)
+#define RREP(i, N) for (int i = ((int)(N)-1); i >= 0; i--)
+#define RREPS(i, N) for (int i = ((int)(N)); i > 0; i--)
+
+#define chmax(ret, x) ret = max(ret, x)
+#define chmin(ret, x) ret = min(ret, x)
+
+#define FILL(x, e) memset(x, e, sizeof(x))
+#define ZEROS(x) fill(x, 0)
+
+#define SORT(x) sort(ALL(x))
+#define RSORT(x) sort(RALL(x))
+
+#define UNIQ(x) x.erase(unique(ALL(x)), x.end())
+
+#define BIT(n) (1LL << (n))
+
+#define OUT(x) cout << (x) << "\n"
+#define DEBUG(x) cerr << #x << " : " << x << "\n"
+#define DEBUG2(x, y) cerr << "(" << #x << ", " << #y << ") = (" << x << ", " << y << ")\n";
+
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define MT make_tuple
+#define FI first
+#define SE second
+
+const int MOD = 1e9 + 7;
+
+void iostream_init() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.setf(ios::fixed);
+  cout.precision(12);
+  // cout.fill('0');
+  // cout.width(3);
+}
+
+int main() {
+  ::iostream_init();
+
+  int Aw, Ab, Bw, Bb, C, D;
+  cin >> Aw >> Ab;
+  cin >> Bw >> Bb;
+  cin >> C >> D;
+
+  if (Ab >= C) {
+    Ab -= C;
+    Bb += C;
+  } else {
+    Aw -= (C - Ab);
+    Bw += (C - Ab);
+    Ab -= C;
+    Bb += C;
+  }
+
+  if (Bw >= D) {
+    Bw -= D;
+    Aw += D;
+  } else {
+    Aw += Bw;
+  }
+
+  OUT(Aw);
+  return 0;
+}

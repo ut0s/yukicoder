dcbb715a578203219687a241038258f95b54f722
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2020-12-28 11:45:09 +0900

    chore: remove porg logging for mac support and update .gitignore

--- bin/install-tools.sh
+++ bin/install-tools.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-# @date Time-stamp: <2019-08-26 23:36:44 tagashira>
 # @file install-tools.sh
 # @brief
 
 DOTFILES="$HOME/dotfiles"
 
 install_oj(){
-  porg -l -p online-judge-tools "pip3 install -U online-judge-tools --user"
+  pip3 install -U online-judge-tools --user
   echo ""
   ln -sf $(cd $(dirname $0) && pwd)/yuki.sh $HOME/bin/yuki && which yuki
   ln -sf $HOME/yukicoder/etc/Makefile $HOME/yukicoder/src/Makefile
 }
 
+main(){
+  install_oj
+}
 
-
-if [ $(which porg) ]; then
-    echo 'porg command found'
-    install_oj
-else
-    echo 'porg command not found ...'
-    eval ${DOTFILES}/etc/init/00-install-porg.sh &&\
-    install_oj
-fi
+main

e6da7d7ef306fd507adcc0002c382441de57a3c8
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2021-05-29 13:48:12 +0900

    feat: support brew gcc update

--- bin/install-tools.sh
+++ bin/install-tools.sh
@@ -11,8 +11,16 @@ install_oj(){
   ln -sf $HOME/yukicoder/etc/Makefile $HOME/yukicoder/src/Makefile
 }
 
+install_brew_gcc(){
+  ln -fs /opt/homebrew/Cellar/gcc/11.1.0_1/bin/g++-11 /usr/local/bin/g++
+  ln -fs /opt/homebrew/Cellar/gcc/11.1.0_1/bin/gcc-11 /usr/local/bin/gcc
+}
+
 main(){
   install_oj
+  if [ "$(uname)" == 'Darwin' ]; then
+    install_brew_gcc
+  fi
 }
 
 main
